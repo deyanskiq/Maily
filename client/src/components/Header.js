157425869b39916df2a8231af4ce7e2a51235f28
Deyan Denchev <deyanddenchev@gmail.com>
2018-10-21 18:33:42 +0300

    Finish authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,44 @@
 import React, {
   Component
 } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        )
+      default:
+        return (
+          <li><a href="/api/logout">Logout</a></li>
+        )
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="left brand-logo">Maily</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">Login With Google</a></li>
+          <Link
+          to={this.props.auth ? '/surveys' : '/'}
+          href="/"
+          className="left brand-logo">
+          Maily
+          </Link>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
           </ul>
         </div>
     </nav>
       )
   }
 }
-export default Header
+
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+export default connect(mapStateToProps)(Header)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,6 @@
 const passport = require('passport')
 
 module.exports = app => {
-
   app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email',
   ] }
   ))
@@ -14,10 +13,16 @@ module.exports = app => {
 
   app.get('/api/logout', (req, res) => {
     req.logout()
-    res.send(req.user)
+    res.redirect('/')
   })
 
-  app.get('/auth/google/callback', passport.authenticate('google'))
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys')
+    })
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user)
   })
