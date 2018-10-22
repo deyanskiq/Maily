b5f837c52fce1a2ac6fd7aa6c291d309f3e53e40
Deyan Denchev <deyanddenchev@gmail.com>
2018-10-22 11:28:41 +0300

    Add changes related to production mode

--- index.js
+++ index.js
@@ -24,4 +24,17 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  // make sure express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'))
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(process.env.PORT || 5000)
