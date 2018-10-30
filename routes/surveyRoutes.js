ff5a95c3829bfcf7b6cf06774b74b76c934ec8ad
Deyan Denchev <deyanddenchev@gmail.com>
2018-10-30 12:33:03 +0200

    get response from sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,13 +1,33 @@
+const _ = require('lodash')
+const Path = require('path-parser').default
+const { URL } = require('url')
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
 const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
+const path = new Path('/api/surveys/:surveyId/:choice')
 
 module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!')
+  })
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = path.test(new URL(url).pathname)
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice }
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value()
+
+    console.log(events)
   })
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
